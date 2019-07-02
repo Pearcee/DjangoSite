f988ef8f67507b2f06c60d30cb82986e380777cc
Steven <sjpearce@gmail.com>
2019-07-02 21:49:26 +0100

    Brunch

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path(r'', views.index,name='index'),
+    path(r'about', views.about,name='about'),
+    path(r'contact', views.contact,name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def about(request):
+    template = 'blog/about.html'
+    return render(request, template)
+
+
+def contact(request):
+    template = 'blog/contact.html'
+    return render(request, template)
+
+
 def index(request):
     template = 'blog/index.html'
     return render(request, template)
