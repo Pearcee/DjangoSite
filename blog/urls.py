7d0f831c8f51dede7b517e46eeb7b4dad9210798
Steven <sjpearce@gmail.com>
2019-07-03 14:24:26 +0100

    Data1

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path(r'', views.index,name='index'),
     path(r'about', views.about,name='about'),
     path(r'contact', views.contact,name='contact'),
+    path(r'data', views.data, name='data'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,46 @@ def contact(request):
 def index(request):
     template = 'blog/index.html'
     return render(request, template)
+
+def data(request, **kwargs):
+    # we will pass this context object into the
+    # template so that we can access the data
+    # list in the template
+    context = {
+        'data': [
+            {
+                'name': 'Celeb 1',
+                'worth': '3567892'
+            },
+            {
+                'name': 'Celeb 2',
+                'worth': '23000000'
+            },
+            {
+                'name': 'Celeb 3',
+                'worth': '1000007'
+            },
+            {
+                'name': 'Celeb 4',
+                'worth': '456789'
+            },
+            {
+                'name': 'Celeb 5',
+                'worth': '7890000'
+            },
+            {
+                'name': 'Celeb 6',
+                'worth': '12000456'
+            },
+            {
+                'name': 'Celeb 7',
+                'worth': '896000'
+            },
+            {
+                'name': 'Celeb 8',
+                'worth': '670000'
+            }
+        ]
+    }
+    template = 'blog/data.html'
+    return render(request, template, context)
